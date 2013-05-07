7a5539eed4786367627970aadaf1e502b43aecee
Mathilde Nygaard Kamperud <mathilde.n.kamperud@gmail.com>
2013-05-07 11:37:04 +0200

    Added submission for project 3

--- project3_folder/find_lw.m
+++
@@ -1,3 +0,0 @@
-function [lw,num] = find_lw(z)
-[lw,num] = bwlabel(z,4);
-end
--- project3_folder/gen_bin_array.m
+++
@@ -1,5 +0,0 @@
-function [bin_array, lw, num] = gen_bin_array(r, p)
-z = r<p;
-[lw,num] = bwlabel(z,4);
-bin_array = z;
-end
